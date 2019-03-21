c18e073d7b9a7a77a3f1ba444edb7c8eb18dab9d
Sergey Lavrov <lavrov-sergey@yandex.ru>
2019-03-21 14:05:49 +0300

    Initial Commit

---
+++ innerupperabilityDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  innerupperabilityDemo
+//
+//  Created by Sergey Lavrov on 21.03.2019.
+//  Copyright © 2019 +1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ innerupperabilityDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  innerupperabilityDemo
+//
+//  Created by Sergey Lavrov on 21.03.2019.
+//  Copyright © 2019 +1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
